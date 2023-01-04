ef29c25fa38c6174874f11ec1bdb90227c3544c4
AlekseyKeks <Aleksey33@gmail.com>
2023-01-04 15:27:08 +0300

    game site

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+const sideBar = document.querySelector('.sidebar'),
+      btn = document.querySelector('.button'),
+      cnt = document.querySelector('.container');
+
+// function change(x) {
+//     x.classList.toggle('change');
+// }
+
+btn.addEventListener('click', () => {
+    // sideBar.classList.toggle('sidebar-new');
+    console.log('привет')
+});
